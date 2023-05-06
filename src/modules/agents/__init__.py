f30141b07c4a460546fca68ccf06c61b982a0890
fahmyadan <fahmy.adan14@imperial.ac.uk>
2023-05-06 16:03:37 +0100

    add agent with more layers

--- src/modules/agents/__init__.py
+++ src/modules/agents/__init__.py
@@ -2,5 +2,7 @@ REGISTRY = {}
 
 from .rnn_agent import RNNAgent
 from .rnn_ns_agent import RNNNSAgent
+from .rnn1_agent import RNN1Agent
 REGISTRY["rnn"] = RNNAgent
 REGISTRY["rnn_ns"] = RNNNSAgent
+REGISTRY['rnn1'] = RNN1Agent
