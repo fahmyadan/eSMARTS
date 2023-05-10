b4dc14ad71799503b6ed69583ae5454eba267364
fahmyadan <fahmy.adan14@imperial.ac.uk>
2023-05-10 10:58:08 +0100

    add cnn agent to registry

--- src/modules/agents/__init__.py
+++ src/modules/agents/__init__.py
@@ -3,6 +3,8 @@ REGISTRY = {}
 from .rnn_agent import RNNAgent
 from .rnn_ns_agent import RNNNSAgent
 from .rnn1_agent import RNN1Agent
+from .cnn_agent import CNNAgent
 REGISTRY["rnn"] = RNNAgent
 REGISTRY["rnn_ns"] = RNNNSAgent
 REGISTRY['rnn1'] = RNN1Agent
+REGISTRY['cnn'] = CNNAgent
