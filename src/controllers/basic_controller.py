e9c6b7cd65fcc8a56a02cff5cdcfc3747cace08c
fahmyadan <fahmy.adan14@imperial.ac.uk>
2023-05-10 17:53:42 +0100

    fix build inputs + shape for 3d image

--- src/controllers/basic_controller.py
+++ src/controllers/basic_controller.py
@@ -74,17 +74,20 @@ class BasicMAC:
                 inputs.append(th.zeros_like(batch["actions_onehot"][:, t]))
             else:
                 inputs.append(batch["actions_onehot"][:, t-1])
-        if self.args.obs_agent_id:
-            inputs.append(th.eye(self.n_agents, device=batch.device).unsqueeze(0).expand(bs, -1, -1))
+       
+        # if self.args.obs_agent_id:
+            # inputs.append(th.eye(self.n_agents, device=batch.device).unsqueeze(0).expand(bs, -1, -1))
 
-        inputs = th.cat([x.reshape(bs*self.n_agents, -1) for x in inputs], dim=1)
+        # inputs = th.cat([x.reshape(bs*self.n_agents, -1) for x in inputs], dim=1)
+
+        inputs = th.cat(inputs, dim=0)
         return inputs
 
     def _get_input_shape(self, scheme):
-        input_shape = scheme["obs"]["vshape"]
+        input_shape = list(scheme["obs"]["vshape"])
         if self.args.obs_last_action:
             input_shape += scheme["actions_onehot"]["vshape"][0]
         if self.args.obs_agent_id:
-            input_shape += (self.n_agents,)
+            input_shape.append(self.n_agents)
 
-        return input_shape
+        return tuple(input_shape)
